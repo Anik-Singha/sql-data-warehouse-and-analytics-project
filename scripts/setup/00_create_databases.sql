c39350ff44cd817592b3c696660c825b75fc6dba
Anik Singha <93642306+Anik-Singha@users.noreply.github.com>
2026-01-31 13:15:55 +0530

    new method

--- scripts/setup/00_create_databases.sql
+++ scripts/setup/00_create_databases.sql
@@ -1,12 +1,39 @@
-/*
-==================================================
- Script Name : 00_create_databases.sql
- Purpose     : Create databases for Medallion Architecture
- Databases   : bronze, silver, gold
- Author      : Anik Singha
-==================================================
+
+/* 
+Creating the database and initializing the schema layers 
+
+Script Purpose - 
+    It is used to create a new database named 'DataWarehouse' inside Microsoft SQL server and 
+    add three schema layers (bronze, silver and gold). 
+
+Warning -
+    This will delete the previous DataWarehouse database if present and all those data will be deleted.
 */
 
-CREATE DATABASE IF NOT EXISTS bronze;
-CREATE DATABASE IF NOT EXISTS silver;
-CREATE DATABASE IF NOT EXISTS gold;
+USE master;
+GO
+
+-- Drop and recreate the 'DataWarehouse' database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouse;
+END;
+GO
+
+-- Create the 'DataWarehouse' database
+CREATE DATABASE DataWarehouse;
+GO
+
+USE DataWarehouse;
+GO
+
+-- Create Schemas
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
