322e1fa0c9389b84e3610b1bfb3e470249776868
Anik Singha <93642306+Anik-Singha@users.noreply.github.com>
2026-02-01 16:03:07 +0530

    m

--- scripts/silver/02_load_silver_data.sql
+++ scripts/silver/02_load_silver_data.sql
@@ -1,84 +1,126 @@
-insert into silver.crm_cust_info (
-	cst_id,
-	cst_key,
-	cst_firstname,
-	cst_lastname,
-	cst_marital_status,
-	cst_gndr,
-	cst_create_date
-)
-
-SELECT 
-	cst_id,
-	cst_key,
-	trim(cst_firstname) as cst_firstname,
-	trim(cst_lastname) as cst_lastname,
-	case when upper(trim(cst_marital_status)) = 'M' then 'Married'
-		 when upper(trim(cst_marital_status)) = 'S' then 'Single'
-		 else 'n/a'
-	end as cst_marital_status,
-	case when upper(trim(cst_gndr)) = 'M' then 'Male'
-		 when upper(trim(cst_gndr)) = 'F' then 'Female'
-		 else 'n/a'
-	end as cst_gndr,
-	cst_create_date
-FROM(
-SELECT 
-*,
-ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS latest_entry
-FROM bronze.crm_cust_info
-)t WHERE latest_entry = 1 
-
+INSERT INTO silver.crm_cust_info
+    (
+        cst_id            ,
+        cst_key           ,
+        cst_firstname     ,
+        cst_lastname      ,
+        cst_marital_status,
+        cst_gndr          ,
+        cst_create_date
+    )
+SELECT
+    cst_id                                   ,
+    cst_key                                  ,
+    trim(cst_firstname) AS cst_firstname     ,
+    trim(cst_lastname)  AS cst_lastname      ,
+    CASE
+        WHEN
+            UPPER(trim(cst_marital_status)) = 'M'
+        THEN 'Married'
+        WHEN
+            UPPER(trim(cst_marital_status)) = 'S'
+        THEN 'Single'
+        ELSE 'n/a'
+    END                 AS cst_marital_status,
+    CASE
+        WHEN
+            UPPER(trim(cst_gndr)) = 'M'
+        THEN 'Male'
+        WHEN
+            UPPER(trim(cst_gndr)) = 'F'
+        THEN 'Female'
+        ELSE 'n/a'
+    END                 AS cst_gndr          ,
+    cst_create_date
+FROM
+    (
+        SELECT
+            *,
+            row_number() OVER
+                (
+                    PARTITION BY
+                        cst_id
+                    ORDER BY
+                        cst_create_date DESC
+                )
+            AS latest_entry
+        FROM
+            bronze.crm_cust_info )t
+WHERE
+    latest_entry = 1
 ---------------------------------------------------
-truncate table silver.crm_prd_info
-INSERT INTO silver.crm_prd_info (
-	prd_id, 
-	cat_id, 
-	prd_key, 
-	prd_nm, 
-	prd_cost, 
-	prd_line, 
-	prd_start_dt, 
-	prd_end_dt
-)
-select 
-	prd_id,
-	REPLACE(substring(prd_key,1,5), '-', '_') as cat_id,
-	REPLACE(substring(prd_key,7,len(prd_key)), '-', '_') as prd_key,
-	prd_nm,
-	ISNULL(prd_cost,0) AS prd_cost,
-	CASE UPPER(TRIM(prd_line))
-		WHEN 'M' THEN 'Mountain'
-		WHEN 'R' THEN 'Road'
-		WHEN 'S' THEN 'Other Sales'
-		WHEN 'T' THEN 'Touring'
-		ELSE 'n/a'
-	END AS prd_line,
-	cast(prd_start_dt as date),
-	cast(LEAD(prd_start_dt) over (partition by prd_key order by prd_start_dt)-1 as date) as prd_end_dt_test
-from bronze.crm_prd_info
-
-
+TRUNCATE TABLE silver.crm_prd_info
+INSERT INTO silver.crm_prd_info
+    (
+        prd_id      ,
+        cat_id      ,
+        prd_key     ,
+        prd_nm      ,
+        prd_cost    ,
+        prd_line    ,
+        prd_start_dt,
+        prd_end_dt
+    )
+SELECT
+    prd_id                                                          ,
+    REPLACE(substring(prd_key,1,5), '-', '_')            AS cat_id  ,
+    REPLACE(substring(prd_key,7,LEN(prd_key)), '-', '_') AS prd_key ,
+    prd_nm                                                          ,
+    ISNULL(prd_cost,0)                                   AS prd_cost,
+    CASE
+        UPPER(trim(prd_line))
+        WHEN
+            'M'
+        THEN 'Mountain'
+        WHEN
+            'R'
+        THEN 'Road'
+        WHEN
+            'S'
+        THEN 'Other Sales'
+        WHEN
+            'T'
+        THEN 'Touring'
+        ELSE 'n/a'
+    END                                                  AS prd_line,
+    CAST(prd_start_dt AS DATE)                                      ,
+    CAST(LEAD(prd_start_dt) OVER
+        (
+            PARTITION BY
+                prd_key
+            ORDER BY
+                prd_start_dt
+        )
+    -1 AS DATE)                                          AS prd_end_dt_test
+FROM
+    bronze.crm_prd_info
 --------------------------------------------
-
-select 
-	sls_ord_num, 
-	sls_prd_key, 
-	sls_cust_id, 
-
-	case when sls_order_dt = 0 or LEN(sls_order_dt) ! = 8 then null
-		 else cast(cast(sls_order_dt as varchar) as date)
-	end as sls_order_dt,
-
-	case when sls_ship_dt = 0 or LEN(sls_ship_dt) ! = 8 then null
-		 else cast(cast(sls_ship_dt as varchar) as date)
-	end as sls_ship_dt,
-
-	case when sls_due_dt = 0 or LEN(sls_due_dt) ! = 8 then null
-		 else cast(cast(sls_due_dt as varchar) as date)
-	end as sls_due_dt,
-
-	sls_sales, 
-	sls_quantity, 
-	sls_price
-from bronze.crm_sales_details
+SELECT
+    sls_ord_num        ,
+    sls_prd_key        ,
+    sls_cust_id        ,
+    CASE
+        WHEN
+            sls_order_dt           = 0
+            OR LEN(sls_order_dt) ! = 8
+        THEN NULL
+        ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
+    END AS sls_order_dt,
+    CASE
+        WHEN
+            sls_ship_dt           = 0
+            OR LEN(sls_ship_dt) ! = 8
+        THEN NULL
+        ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE)
+    END AS sls_ship_dt ,
+    CASE
+        WHEN
+            sls_due_dt           = 0
+            OR LEN(sls_due_dt) ! = 8
+        THEN NULL
+        ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
+    END AS sls_due_dt  ,
+    sls_sales          ,
+    sls_quantity       ,
+    sls_price
+FROM bronze.crm_sales_details
