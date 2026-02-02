5f72ffe6f6360e2f59f6cab1eaa8e0b08ed2923b
Anik Singha <93642306+Anik-Singha@users.noreply.github.com>
2026-02-02 19:22:46 +0530

    erp_cust_az12

--- scripts/silver/02_load_silver_data.sql
+++ scripts/silver/02_load_silver_data.sql
@@ -152,3 +152,23 @@ SELECT
     END AS sls_price       -- Derive price if original value is invalid
 FROM
     bronze.crm_sales_details;
+
+--------------------------------------
+
+insert into silver.erp_cust_az12(
+    cid,
+    bdate,
+    gen
+)
+select 
+    case when cid like 'NAS%' then SUBSTRING(cid,4,LEN(cid))
+        else cid
+    end cid,
+    case when bdate > GETDATE() then null
+        else bdate
+    end bdate,  -- Set future birthdates to NULL
+    case when upper(trim(gen)) in ('F', 'Female') then 'Female'
+         when upper(trim(gen)) in ('M', 'Male') then 'Male'
+         else 'n/a'
+    end as gen  -- Normalize gender values and handle unknown cases
+from bronze.erp_cust_az12
