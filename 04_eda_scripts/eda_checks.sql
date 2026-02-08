eaf6c65deba94ce2d5478ce27bfc1017fc17f2e3
Anik Singha <93642306+Anik-Singha@users.noreply.github.com>
2026-02-08 14:54:02 +0530

    u

--- 04_eda_scripts/eda_checks.sql
+++ 04_eda_scripts/eda_checks.sql
@@ -162,3 +162,78 @@ ON f.customer_key = c.customer_key
 GROUP BY
 	c.country
 ORDER BY total_sold_items DESC; 
+
+-- RANKINGS
+-- =====================================
+
+-- Which 5 products generate the highest revenue
+SELECT TOP 5
+	p.product_name,
+	SUM(f.sales) total_revenue
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+ON p.product_key = f.product_key
+GROUP BY p.product_name
+ORDER BY total_revenue DESC;
+
+-- OR
+
+SELECT TOP 5
+    p.product_name,
+    SUM(f.sales) AS total_revenue
+FROM gold.dim_products p
+INNER JOIN gold.fact_sales f -- Changed to INNER JOIN
+    ON p.product_key = f.product_key
+GROUP BY p.product_name
+ORDER BY total_revenue DESC;
+
+-- 5 worst performing products in terms of sales
+
+SELECT TOP 5
+	p.product_name,
+	SUM(f.sales) total_revenue
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+ON p.product_key = f.product_key
+GROUP BY p.product_name
+ORDER BY total_revenue ASC;
+-- OR
+SELECT *
+FROM
+(SELECT TOP 5
+	p.product_name,
+	SUM(f.sales) total_revenue,
+	ROW_NUMBER() OVER (ORDER BY SUM(f.sales) DESC) AS rank_products
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+ON p.product_key = f.product_key
+GROUP BY p.product_name) t
+WHERE rank_products <= 5;
+
+-- FIND TOP 10 CUSTOMERS WHO HAVE GENERATED THE HIGHEST REVENUE
+SELECT TOP 10
+    c.customer_key,
+    CONCAT(c.first_name, ' ', c.last_name) AS full_name,
+    SUM(f.sales) AS total_revenue
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_customers c
+    ON c.customer_key = f.customer_key
+GROUP BY 
+    c.customer_key,
+    CONCAT(c.first_name, ' ', c.last_name)
+ORDER BY total_revenue DESC;
+
+-- 3 customers with fewest orders placed
+SELECT TOP 3
+    c.customer_key,
+    c.first_name,
+	c.last_name,
+    COUNT(DISTINCT order_number) AS total_orders
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_customers c
+    ON c.customer_key = f.customer_key
+GROUP BY 
+    c.customer_key,
+    c.first_name,
+	c.last_name
+ORDER BY total_orders ASC;
